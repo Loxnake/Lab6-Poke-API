db2ea6faeeca1fc199c21188244d0608973e3b3a
Loxnake <98180288+Loxnake@users.noreply.github.com>
2022-03-24 16:07:16 -0400

    Completed project
    was trying for a while to make line 26
    "for len(pokeDict['abilities])"
    then I realized I needed a variable to iterate so I tried
    ""for i in len(pokeDict['abilities])"
    which still didnt work so I googled how to use for loops to refresh my memory and they had an example using a range so I tried something I didn't think would work but it did and now I'm happy
    *pay no attention to the typo on line 20*

--- script.py
+++ script.py
@@ -1,4 +1,59 @@
+import requests
+from sys import argv
+
 def main():
-    pass
+
+    pokeInfo = retrieveMon(argv[1])
+    if retrieveMon:
+        pastingStrings = pasteBinStrings(pokeInfo)
+        pasteBinURL = pasteBinPOST(pastingStrings[0], pastingStrings[1])
+
+        print(pasteBinURL)
+
+def retrieveMon(pokeName):
+    response = requests.get('https://pokeapi.co/api/v2/pokemon/' + str(pokeName))
+    print('Getting Pokemon Information...', end = '')
+    if response.status_code == 200:
+     print('Success!')
+     return response.json()
+    else:
+     print('Fialed, Response code:',response.status_code)
+     return
+    
+def pasteBinStrings(pokeDict):
+
+    body = ""
+    for i in range(len(pokeDict['abilities'])):
+        body += "-" + pokeDict['abilities'][i]['ability']['name'] + '\n'
+
+
+    title = pokeDict['name'] + "'s Abilities"
+
+    #body += " " + pokeDict['address']['geo']['lat']+ "\n"
+    #body += "Longitude: "+ pokeDict['address']['geo']['lng']
+
+    return (title, body)
+
+def pasteBinPOST(title, body):
+
+    
+    params = {
+        'api_dev_key': "f4R0OTFza_qTQ1NZJYLjoCeLqoHQux4X",
+        'api_option': 'paste',
+        'api_paste_code': body,
+        'api_paste_name': title
+    }
+    response = requests.post('https://pastebin.com/api/api_post.php', data=params)
+
+    print('Posting to PasteBin...', end = '')
+
+    if response.status_code == 200:
+     print('Success!')
+     return response.text
+    else:
+     print('Failed, Response code:',response.status_code)
+     return response.status_code
+
+
 
 main()
